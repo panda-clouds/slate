c61ca8d571267d79e90be3831d16207b337b05ab
Marc Smith <mr.marcsmith@me.com>
2020-02-24 14:52:14 -0700

    gem install bundler

--- local.sh
+++ local.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
+# Only install Bundler if it's not present
+t=`which bundle`
+[ -z "$t" ] && gem install bundler
+
+# Install dependencies if needed
 bundle install
+
+# start the development server locally
 bundle exec middleman server
